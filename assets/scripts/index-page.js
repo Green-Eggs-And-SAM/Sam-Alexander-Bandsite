df63f000b086c16a76c5617fe33d88cf3a4835dd
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-04-15 20:02:57 -0700

    Comment submission form added

---
+++ assets/scripts/index-page.js
@@ -0,0 +1,9 @@
+class userComment {
+    constructor(name, comment, timeStamp) {
+        this.name = name;
+        this.comment = comment;
+        this.timeStamp = timeStamp;
+    }
+}
+
+function addNewComment() {}
