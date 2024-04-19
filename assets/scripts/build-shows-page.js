6bd8cf09e37d489ad68c4c1a2d7db46174fe89d8
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-04-18 21:31:29 -0700

    shows javascript added

--- assets/scripts/build-shows-page.js
+++ assets/scripts/build-shows-page.js
@@ -31,13 +31,68 @@ const shows = [
     },
 ];
 
-function createNewElement(tag, classes, contentText) {
-    let newEl = document.createElement(tag.textContent);
-    if (contentText != undefined) {
-        newEl.textContent = contentText;
-    }
-    if (classes != undefined) {
-        newEl.classList.add(classes);
-    }
+const insertShowHere = document.getElementById('list-of-shows');
+displayShows();
+
+function displayShows() {
+    shows.forEach((show) => insertShowHere.append(createShowCard(show)));
+}
+
+function createShowCard(showToAdd) {
+    let show = document.createElement('article');
+
+    let newDateInfo = createInfoDiv('DATE', showToAdd.date, true);
+    // console.log(newInfo.innerHTML);
+    show.appendChild(newDateInfo);
+
+    let newVenueInfo = createInfoDiv('VENUE', showToAdd.venue, false);
+    show.appendChild(newVenueInfo);
+
+    let newLocationInfo = createInfoDiv('LOCATION', showToAdd.location, false);
+    show.appendChild(newLocationInfo);
+    show.classList.add('shows__card');
+
+    let newButton = createButton();
+    show.appendChild(newButton);
+    return show;
+}
+
+function createButton() {
+    let newButton = document.createElement('button');
+    newButton.classList.add('shows__get-tix-button');
+
+    let newButtonText = document.createElement('h5');
+    newButtonText.textContent = 'BUY TICKETS';
+    newButtonText.classList.add('shows__get-tix-button--text');
+    newButton.appendChild(newButtonText);
+
+    return newButton;
+}
+
+function createInfoDiv(subheaderText, bodyText, isBold) {
+    let infoDiv = document.createElement('div');
+    infoDiv.classList.add('shows__card--info');
+
+    let subheader = createNewTextElement(
+        'p',
+        'shows__card--subheader',
+        subheaderText
+    );
+    infoDiv.appendChild(subheader);
+
+    let text = createNewTextElement('h5', 'shows__card--text', bodyText);
+    if (isBold) text.classList.add('bold-text');
+
+    infoDiv.appendChild(text);
+
+    return infoDiv;
+}
+
+function createNewTextElement(tag, classes, contentText) {
+    let newEl = document.createElement(tag);
+
+    if (contentText != undefined) newEl.textContent = contentText;
+    if (classes != undefined) newEl.classList.add(classes);
+
     return newEl;
 }
